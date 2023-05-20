d2600e7a394ca4f11e4e4d82098e2654d79d6dc0
AlaaBK <alaa.benkhalifa@esprit.tn>
2023-05-20 13:00:30 +0100

    commit

---
+++ src/test/java/com/ileadconsultiong/projectCoding/ProjectCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ileadconsultiong.projectCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
